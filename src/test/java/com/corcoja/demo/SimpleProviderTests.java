210fade768320d6ba91c3e156eb77b3fd6cc5a18
Iulian Corcoja <iulian@corcoja.com>
2022-06-10 17:43:29 +0300

    Update `SimpleProvider` unit tests

--- src/test/java/com/corcoja/demo/SimpleProviderTests.java
+++ src/test/java/com/corcoja/demo/SimpleProviderTests.java
@@ -13,19 +13,19 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 import org.awaitility.Awaitility;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import com.corcoja.demo.protocol.Provider;
 
 public class SimpleProviderTests {
 
-    @Test
-    public void testProviderConcurrentRequests() {
+    @ParameterizedTest
+    @ValueSource(ints = {0, 5, 9, 10, 11, 16, 32})
+    public void testProviderConcurrentRequests(Integer requests) {
         Provider provider = Utils.createSimpleProvider();
-
-        Integer requests = 16;
 
         Float maxLoad =
                 Math.min(1.0f, (float) requests / (float) Constants.providerMaxConcurrentRequests);
-
         Duration processingWaitTime =
                 Constants.providerRequestProcessingTime.multipliedBy(1 + (long) Math
                         .ceil((float) requests / (float) Constants.providerMaxConcurrentRequests));
@@ -62,21 +62,26 @@ public class SimpleProviderTests {
     public void testProviderAvailability() {
         Provider provider = Utils.createSimpleProvider();
 
-        // Set provider as unavailable
+        System.out.println("Set provider as unavailable");
+
         provider.setAvailability(false);
 
-        // Provider must be unavailable when invoking check method
+        System.out.println("Check provider is unavailable when invoking `check` method");
+
         CompletableFuture<Boolean> checkFuture = CompletableFuture.supplyAsync(provider::check);
         assertThrows(TimeoutException.class, () -> checkFuture.get(2, TimeUnit.SECONDS));
 
-        // Provider must be also unavailable when invoking get method
+        System.out.println("Check provider is unavailable when invoking `get` method");
+
         CompletableFuture<String> getFuture = CompletableFuture.supplyAsync(provider::get);
         assertThrows(TimeoutException.class, () -> getFuture.get(2, TimeUnit.SECONDS));
 
-        // Set provider as available
+        System.out.println("Set provider as available");
+
         provider.setAvailability(true);
 
-        // Now all requests to provider must work and return immediately
+        System.out.println("All requests to provider must work and return immediately");
+
         assertDoesNotThrow(() -> assertEquals(Boolean.TRUE, checkFuture.get()));
         assertDoesNotThrow(() -> assertNotNull(getFuture.get()));
     }
